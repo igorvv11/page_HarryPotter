d7526772a5cc83e6db858c2e307981d9cae95cd0
igor viana <igor.viana560@gmail.com>
2024-05-08 15:21:35 -0300

    primeiro commit

---
+++ src/scripts/main.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]')
+
+    const heroSection = document.querySelector('.hero')
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if( posicaoAtual < alturaHero){
+            ocultaElementoDoHeader();
+        } else {
+            mostraElementoDoHeader();
+        }
+    })
+    
+    //Seção de atrações, programação das abas.
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao){
+            const abaAlvo = botao.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
+            escondeTodasAba();
+            aba.classList.add('shows__list--is-active');
+            removeBotaoAtivo();
+            botao.target.classList.add('shows__tabs__button--is-active')
+        })
+    }
+
+//Seção Faq accordions
+    for (let i = 0; i < questions.length; i++) { 
+        questions[i].addEventListener('click', abreOufechaResposta);
+    
+    }
+})
+
+function ocultaElementoDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+function mostraElementoDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+
+function abreOufechaResposta (elemento){
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
+
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active')
+    }
+
+
+}
+
+function escondeTodasAba() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++ ) {
+        tabsContainer[i].classList.remove('shows__list--is-active');
+
+    }
+}
